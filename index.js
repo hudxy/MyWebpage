e1b14d3ded11d91b7c2646fe89eb83c30447bc7f
Hudson <hudsonxy@gmail.com>
2018-12-19 16:25:57 -0800

    Trying to add arrow key inputs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+var image3 = $(".image3");
+
+
+function move(e) {
+
+  if(e.keyCode == 40) {
+    image3 =width:400px;height:800px;
+  }
+}
