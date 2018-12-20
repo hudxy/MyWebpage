bcea6af59fb6857a85dbb5a629039128fc37246a
Hudson <hudsonxy@gmail.com>
2018-12-20 12:55:29 -0800

    setTimeout used to hide images after different times
    hide images after 3, 4, and 5 seconds. Show button will now show all images

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 
 $(document).ready(function(){
   $("#hide").click(function(){
-    $(".image3").hide();
+    $("#image3").hide();
     });
   $("#show").click(function(){
-    $(".image3").show();
+    $(".image").show();
   });
 });
 
@@ -15,25 +15,28 @@ $(document).ready(function(){
 $(document).keydown(function(e) {
     switch (e.which) {
     case 37:
-        $('.image3').animate({
+        $("#image3").animate({
             left: '-=50px'
         });
         break;
     case 38:
-        $('.image3').background
-        $('.image3').animate({
+        $("#image3").animate({
             top: '-=50px'
         });
         break;
     case 39:
-        $('.image3').animate({
+        $("#image3").animate({
             left: '+=50px'
         });
         break;
     case 40:
-        $('.image3').animate({
+        $("#image3").animate({
             top: '+=50px'
         });
         break;
     }
 })
+
+setTimeout(function(){ $("#image1").hide(); }, 3000);
+setTimeout(function(){ $("#image2").hide(); }, 4000);
+setTimeout(function(){ $("#image3").hide(); }, 5000);
