fc07fbfc937c0d321e6d810e3bbb5a051248f99d
Hudson <hudsonxy@gmail.com>
2018-12-21 18:16:46 -0800

    Using newly created event method

--- index.js
+++ index.js
@@ -52,23 +52,32 @@ function showHide(h) {
   }
 }
 
-//Create new Event
-//var event = new CustomEvent('myEvent', {'detail': 'Example event'});
-
-
-
-image1.addEventListener('click', function() {showHide(image1);});
-image2.addEventListener('click', function() {showHide(image2);});
-image3.addEventListener('click', function() {showHide(image3);});
-
-setInterval(function(){$('#image1').trigger('click');}, 3000);
-setInterval(function(){$('#image2').trigger('click');}, 4000);
-setInterval(function(){$('#image3').trigger('click');}, 5000);
-
-
-
-
 //Hide images after so many milliseconds
 //setTimeout(function(){ $("#image1").hide(); }, 3000);
 //setTimeout(function(){ $("#image2").hide(); }, 4000);
 //setTimeout(function(){ $("#image3").hide(); }, 5000);
+
+
+//Event Listeners that show/hide images based on click event
+//image1.addEventListener('click', function() {showHide(image1);});
+//image2.addEventListener('click', function() {showHide(image2);});
+//image3.addEventListener('click', function() {showHide(image3);});
+
+
+//Using jQuery trigger() function to simulate click event.
+//setInterval(function(){$('#image1').trigger('click');}, 3000);
+//setInterval(function(){$('#image2').trigger('click');}, 4000);
+//setInterval(function(){$('#image3').trigger('click');}, 5000);
+
+//Create new Event
+var newEvent = new Event('myEvent');
+
+//Event Listeners that show/hide images based a newly created event
+image1.addEventListener('myEvent', function() {showHide(image1);});
+image2.addEventListener('myEvent', function() {showHide(image2);});
+image3.addEventListener('myEvent', function() {showHide(image3);});
+
+//Uses dispatchEvent function to send newly created event to given element
+setInterval(function(){image1.dispatchEvent(newEvent);}, 3000);
+setInterval(function(){image2.dispatchEvent(newEvent);}, 4000);
+setInterval(function(){image3.dispatchEvent(newEvent);}, 5000);
